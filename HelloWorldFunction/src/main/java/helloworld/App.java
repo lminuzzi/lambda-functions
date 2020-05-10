772f6b8684993544165e66ce1ca1aa3cb9d16068
Luciano Minuzzi <lucianopminuzzi@gmail.com>
2020-05-10 20:13:32 +0200

    - Create docs folder; - Add handy-tool-catalog.json file; - Read handy-tool-catalog.json file from the Bucket;

--- HelloWorldFunction/src/main/java/helloworld/App.java
+++ HelloWorldFunction/src/main/java/helloworld/App.java
@@ -19,18 +19,13 @@ public class App implements RequestHandler<String, Object> {
     public Object handleRequest(final String input, final Context context) {
         context.getLogger().log("Input = " + input);
         Region region = Region.EU_WEST_3;
+        S3Client s3Client = S3Client.builder().region(region).build();
 
-        S3Client s3Client = S3Client.builder()
-                .region(region)
-                .build();
-
-        ResponseInputStream<?> responseInputStream = s3Client.getObject(
-                GetObjectRequest
-                        .builder()
-                        .bucket("mastermadness-postkey-data")
-                        .key("s3textdata.txt")
-                        .build()
-        );
+        ResponseInputStream<?> responseInputStream = s3Client
+                .getObject(GetObjectRequest.builder()
+                        .bucket("lpm-inventory-data")
+                        // .key("s3testdata.txt")
+                        .key("handy-tool-catalog.json").build());
 
         InputStreamReader inputStreamReader = new InputStreamReader(responseInputStream);
         BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
@@ -44,28 +39,5 @@ public class App implements RequestHandler<String, Object> {
             context.getLogger().log("An exception was generated when attempting to readLine() BufferedReader");
         }
         return outputStream;
-
-
-        /*//Generated code
-        Map<String, String> headers = new HashMap<>();
-        headers.put("Content-Type", "application/json");
-        headers.put("X-Custom-Header", "application/json");
-        try {
-            final String pageContents = this.getPageContents("https://checkip.amazonaws.com");
-            String output = String.format("{ \"message\": \"hello world\", \"location\": \"%s\" }", pageContents);
-            return new GatewayResponse(output, headers, 200);
-        } catch (IOException e) {
-            return new GatewayResponse("{}", headers, 500);
-        }
-        */
     }
-
-    /*
-    private String getPageContents(String address) throws IOException{
-        URL url = new URL(address);
-        try(BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()))) {
-            return br.lines().collect(Collectors.joining(System.lineSeparator()));
-        }
-    }
-    */
 }
